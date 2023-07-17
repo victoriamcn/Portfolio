96b6ad9ed08086c98f2ddabe915b00007c26bf76
Victoria <vmcnorrill@gmail.com>
2023-07-16 22:19:55 -0400

    build

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import vue from '@vitejs/plugin-vue'
+
 
 // https://vitejs.dev/config/
+/** @type {import('vite').UserConfig} */
 export default defineConfig({
-  plugins: [react()],
-  base:'./',
+  base: '/Portfolio/',
+  plugins: [react(), vue()],
 })
