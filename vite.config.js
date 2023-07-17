e99d03e33b114bd0f357fad1108ace5ff85f0f8b
Victoria <vmcnorrill@gmail.com>
2023-07-17 19:57:48 -0400

    added margin on all /pages

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,6 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 /** @type {import('vite').UserConfig} */
 export default defineConfig({
-  base: '/Portfolio/',
+  base: '/',
   plugins: [react(), vue()],
 })
